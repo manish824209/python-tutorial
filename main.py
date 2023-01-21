070a910b89943343bd77bc6e47ab4fd90ac28e7b
manish824209 <kumarmanish824209@gmail.com>
2023-01-22 03:50:58 +0530

    till operator completed

---
+++ main.py
@@ -0,0 +1,203 @@
+# a=int(input())
+# b=int(input())
+# print(f'sum of {a}+{b} =', a+b)
+# print(f'sub {a}-{b} =',   a-b)
+# print(f'multi {a}*{b} =',a *b)
+# print(f'div  {a}\{b} =',a//b)
+# # #WAP to check a user can vote or not. if he can vote print yes, if not print no. condition age is must more than 18
+# # # a=int (input())
+# # # if a>18:
+# # #     print("yes")
+# # # else:
+# # #     print("no")
+# # # a=int (input())
+# # # b=(input())
+# # # if a>18 and a<70 and b=='m':
+# # #     print("yes")
+# # # elif a>18 and a<60 and b=='f':
+# # #     print("yes")
+# # # else :
+# # #     print("no")
+# # #
+# # # a=int(input())
+# # # b=int(input())
+# # # if a>b:
+# # #     print("a")
+# # # else:
+# # #     print("b")
+# # # a=int (input())
+# # # b=int(input())
+# # # c=int(input())
+# # # d=int(input())
+# # # if a>b and a>c and a>d:
+# # #     print("a")
+# # # elif b>c and b>d:
+# # #     print("b")
+# # # elif c>d:
+# # #     print("c")
+# # # else :
+# # #     print("d")
+# # #
+# # # a=int (input())
+# # # b=int(input())
+# # # c=int(input())
+# # # d=int(input())
+# # # if a<b and a<c and a<d:
+# # #    print("a")
+# # # elif b<c and b<d:
+# # #    print("b")
+# # # elif c<d:
+# # #    print("c")
+# # # else :
+# # #    print("d")
+# # # a=int (input())
+# # # b=int(input())
+# # # c=int (input())
+# # # d=int (input())
+# # # print(max (a,b,c,d))
+# # # print(min (a,b,c,d))
+# # #
+# # # a=int(input())
+# # # if a%2==0:
+# # #    print("even")
+# # # else:
+# # #     print("odd")
+# #
+# # '''a =input()
+# # b= input()
+# # c= input()
+# # d= input()
+# # print(type(a))'''
+# # #print("hello world")
+# # #print('ravi')
+# # #print('''"ravi's book"''')
+# # # print("9+9")
+# # # a=9
+# # # b=9
+# # # print(a+b)
+# # # a=9
+# # # print (type(a))
+# # # '''a= int(input())
+# # # b= int(input())
+# # # print (min(a,b))'''
+# # name=(input())
+# # if name=="pinky":
+# #     print("i love you")
+# #
+# # else:
+# #a = int(input("",\%r))
+# #print(a)
+#
+# # a=(input())
+# # if a.isupper():
+# #     print("yesh")
+# # else:
+# #     print("no")
+# a=str(input())
+# b=str(input())
+# c=str(input())
+# d=str(input())
+# if a>c:
+#     print('yes')
+# if a>b and a>c and a>d:
+#     print('a')
+# elif b>c and b>d:
+#     print("b")
+# elif c>d:
+#     print("c")
+# else:
+#     print("d")
+#
+# a=int(input())
+# if a%2!=0:
+#     print('weard')
+# else:
+#     if 2<a<5:
+#         print('weard')
+#     elif 6<a<20:
+#         print('not weard')
+#     else:
+#         print('weard')
+#a=int(input("enter start number "))
+#b=int(input(" end number"))
+
+# for i in range (a,b,-1): #negative loop
+#     print(i,end=" ")
+# a=int(input("enter start number "))# positive loop
+# b=int(input(" end number"))
+# for i in range (a,b +1):
+#     print(i,end=" ")
+
+# n=int(input())
+# for i in range(0,n+0):
+#     print(i*i)
+# n=int(input())
+# n=int(input())
+# for i in range(1,10+1):
+#     print(i)
+# for i in range(10,0,-1):
+#     print(i)
+# for i in range (1,101,1):# break loop
+#     if i==6:
+#         break
+#     else:
+#         print(i)
+# n=int(input())  # leap year nd not leap year
+# if n%4==0:
+#     if n%100==0:
+#         if n%400==0:
+#             print("leap")
+#         else:
+#             print i % 2 =! 0:int("not leap")
+#     else :
+#         print("leap")
+# else:
+#     print("not leap")
+
+
+# def manish(name):
+#     print(f'hello {name}')
+# name=str(input())
+# manish(name)
+# manish(name)
+# manish(name)
+# manish(name)
+# def manish (name):
+#     print(f'hello {name}')
+# name=str(input())
+# for i in range(0,10):
+#     manish(name)
+# Python program to print Even Numbers in given range
+
+# a= int(input("frist no= "))
+# b = int(input("second no= "))
+# for i  in range(a,b + 1):
+#  	if i   % 2 == 0 :
+#  		print( i  , end=" ")
+
+
+# a = int(input("frist no= "))
+# b = int(input("second no= "))
+#
+# for i in range(a , b + 1):
+#
+# 		print(i, end=" ")
+
+# a=int(input("frist no="))
+# b=int(input("frist no="))
+# for i in range (a,b +1):
+#     if i %2!= 0:
+#         print(i,end=" ")
+# def nitish(name):
+#     return("manish")
+# a= nitish("manish")
+# print(a)
+
+sum=0
+a=int(input())
+b=int(input())
+for i in range (a,b+1):
+     sum+=i
+print(sum)
+
+
